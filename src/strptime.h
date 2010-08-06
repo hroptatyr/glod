a533631debb90defcf30940c7af524d0874bd0ae
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2010-08-06 13:19:33 +0000

    new, import cell type predicates for date guessing
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

---
+++ src/strptime.h
@@ -0,0 +1,93 @@
+/*** strptime.h -- faster date parsing
+ *
+ * Copyright (C) 2009, 2010  Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <sebastian.freundt@ga-group.nl>
+ *
+ * This file is part of libffff.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+
+#if !defined INCLUDED_strptime_h_
+#define INCLUDED_strptime_h_
+
+#undef FDECL
+#undef FDEFU
+#if !defined STATIC_GUTS
+# define FDECL		extern
+# define FDEFU
+#else  /* STATIC_GUTS */
+# define FDECL		static
+# define FDEFU		static
+#endif	/* !STATIC_GUTS */
+
+FDECL void ffff_strptime(const char *buf, struct tm *restrict tm);
+FDECL void ffff_strptime_ISO(const char *buf, struct tm *restrict tm);
+FDECL void ffff_strptime_Ymd(const char *buf, struct tm *restrict tm);
+/* for the glod stuff */
+FDECL int
+glod_strptime(const char *buf, const char *fmt, struct tm *restrict tm);
+
+
+/**
+ * Return whether YEAR is a leap year. */
+static inline int __attribute__((always_inline))
+__leapp(int year)
+{
+#if 0
+	/* actually this is */
+	return (year % 4 == 0 && ((year % 100) || (year % 400 == 0)));
+#else
+	/* but our design range (1970 to 2099) has no bullshit years in it */
+	return (year % 4) == 0;
+#endif
+}
+
+/**
+ * Return the number of leap years in [1970, YEAR). */
+static inline int __attribute__((always_inline))
+__nleap_years(int year)
+{
+	/* again, we (ab)use that our design range is 1970 to 2099 and
+	 * that it has no bullshit years in it */
+	return (year - 1970 + 1) / 4;
+}
+
+#if defined INCL_TBLS
+/* data */
+static const unsigned short int __attribute__((unused)) __mon_yday[2][13] = {
+	/* Normal years.  */
+	{ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 },
+	/* Leap years.  */
+	{ 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 }
+};
+#endif	/* INCL_TBLS */
+
+#endif	/* INCLUDED_strptime_h_ */
