5470fc370d4c7f3bcb889c000b150f3b66c515ce
Sebastian Freundt <freundt@ga-group.nl>
2013-07-23 08:09:49 +0000

    fix, PEBKAC, use actual name of build-df script in calc-tf
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/calc-tf.sh
+++ src/calc-tf.sh
@@ -3,7 +3,7 @@
 ## Usage: tf-idf.sh CORPUS FILE
 
 cat "${2}" | \
-	$(dirname "${0}")/build-rel.sh | \
+	$(dirname "${0}")/build-df.sh | \
 	join -t'	' -j2 - "${1}" | \
 	awk 'BEGIN{FS=OFS="\t"}{print sprintf("%.6f", $2 / $3), $1;}' | \
 	sort -nr
