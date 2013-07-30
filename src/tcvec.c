f47424baefe6a450e257d636ec78908355a7663c
Sebastian Freundt <freundt@ga-group.nl>
2013-07-30 15:42:11 +0000

    minor, provide command line tool to add terms to the corpus, tcvec(1)
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ src/tcvec.c
@@ -0,0 +1,107 @@
+/*** tcvec.c -- return a term-count vector
+ *
+ * Copyright (C) 2013 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of glod.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "glkv.h"
+
+
+static void
+snarf(glkv_t ctx)
+{
+	char *line = NULL;
+	size_t llen = 0U;
+	ssize_t nrd;
+
+	while ((nrd = getline(&line, &llen, stdin)) > 0) {
+		gl_crpid_t id;
+
+		line[nrd - 1] = '\0';
+		if ((id = glkv_add_term(ctx, line))) {
+			;
+		} else {
+			fprintf(stderr,
+				"Error: putting `%s' into corpus\n", line);
+			break;
+		}
+		printf("%u\n", id);
+	}
+	free(line);
+	return;
+}
+
+
+#if defined __INTEL_COMPILER
+# pragma warning (disable:593)
+# pragma warning (disable:181)
+#endif	/* __INTEL_COMPILER */
+#include "tcvec.xh"
+#include "tcvec.x"
+#if defined __INTEL_COMPILER
+# pragma warning (default:593)
+# pragma warning (default:181)
+#endif	/* __INTEL_COMPILER */
+
+int
+main(int argc, char *argv[])
+{
+	struct glod_args_info argi[1];
+	glkv_t ctx;
+	int res;
+
+	if (glod_parser(argc, argv, argi)) {
+		res = 1;
+		goto out;
+	}
+
+	if ((ctx = make_glkv("/tmp/glkv.tcb", O_RDWR | O_CREAT)) == NULL) {
+		goto out;
+	}
+
+	/* just categorise the whole shebang */
+	snarf(ctx);
+
+	free_glkv(ctx);
+out:
+	glod_parser_free(argi);
+	return res;
+}
+
+/* tcvec.c ends here */
