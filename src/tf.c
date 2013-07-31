75da3269b89ac79f1579b4f7158fd8b5f44dcfde
Sebastian Freundt <freundt@ga-group.nl>
2013-07-31 09:43:44 +0000

    minor, actually output the term frequency
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/tf.c
+++ src/tf.c
@@ -39,13 +39,67 @@
 #endif	/* HAVE_CONFIG_H */
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <fcntl.h>
 #include "corpus.h"
 #include "nifty.h"
 
+typedef struct ctx_s ctx_t[1];
+typedef struct tf_s *tf_t;
+
+struct ctx_s {
+	gl_corpus_t c;
+	tf_t tf;
+};
+
+struct tf_s {
+#define TF_MAX	((sizeof(uint_fast8_t) << 8U) - 1U)
+	size_t nf;
+	uint_fast8_t f[];
+};
+
 
 static void
-snarf_add(gl_corpus_t ctx)
+resize_tf(ctx_t ctx, gl_crpid_t id)
+{
+#define TF_RESZ_STEP	(64U)
+	size_t ol;
+
+	if (UNLIKELY(ctx->tf == NULL)) {
+		ol = 0U;
+	} else if (UNLIKELY((ol = ctx->tf->nf) <= id)) {
+		;
+	} else {
+		return;
+	}
+	/* resize then */
+	with (size_t nu = (ol + TF_RESZ_STEP)) {
+		ctx->tf = realloc(
+			ctx->tf, nu * sizeof(*ctx->tf->f) + sizeof(*ctx->tf));
+		/* bzero */
+		memset(ctx->tf->f + ol, 0, TF_RESZ_STEP * sizeof(*ctx->tf->f));
+		ctx->tf->nf = nu;
+	}
+	return;
+}
+
+static void
+rec_t(ctx_t ctx, gl_crpid_t id)
+{
+/* record term (identified by its ID) */
+
+	/* check if stuff is big enough */
+	resize_tf(ctx, id);
+	/* just inc the counter */
+	if (LIKELY(ctx->tf->f[id] < TF_MAX)) {
+		ctx->tf->f[id]++;
+	}
+	return;
+}
+
+static void
+snarf_add(ctx_t ctx)
 {
 	char *line = NULL;
 	size_t llen = 0U;
@@ -55,21 +109,21 @@ snarf_add(gl_corpus_t ctx)
 		gl_crpid_t id;
 
 		line[nrd - 1] = '\0';
-		if ((id = corpus_add_term(ctx, line))) {
+		if ((id = corpus_add_term(ctx->c, line))) {
 			;
 		} else {
 			fprintf(stderr,
 				"Error: putting `%s' into corpus\n", line);
 			break;
 		}
-		printf("%u\n", id);
+		rec_t(ctx, id);
 	}
 	free(line);
 	return;
 }
 
 static void
-snarf_get(gl_corpus_t ctx)
+snarf_get(ctx_t ctx)
 {
 	char *line = NULL;
 	size_t llen = 0U;
@@ -79,8 +133,8 @@ snarf_get(gl_corpus_t ctx)
 		gl_crpid_t id;
 
 		line[nrd - 1] = '\0';
-		id = corpus_get_term(ctx, line);
-		printf("%u\n", id);
+		id = corpus_get_term(ctx->c, line);
+		rec_t(ctx, id);
 	}
 	free(line);
 	return;
@@ -103,7 +157,7 @@ main(int argc, char *argv[])
 {
 	struct glod_args_info argi[1];
 	const char *db = GLOD_DFLT_CORPUS;
-	gl_corpus_t ctx;
+	static struct ctx_s ctx[1];
 	int oflags = 0;
 	int res;
 
@@ -119,9 +173,12 @@ main(int argc, char *argv[])
 	if (argi->add_given) {
 		oflags = O_RDWR | O_CREAT;
 	}
-	if (UNLIKELY((ctx = make_corpus(db, oflags)) == NULL)) {
+	if (UNLIKELY((ctx->c = make_corpus(db, oflags)) == NULL)) {
 		goto out;
 	}
+
+	/* initialise the freq vector */
+	ctx->tf = NULL;
 
 	/* just categorise the whole shebang */
 	if (argi->add_given) {
@@ -130,7 +187,12 @@ main(int argc, char *argv[])
 		snarf_get(ctx);
 	}
 
-	free_corpus(ctx);
+	for (size_t i = 0; i < ctx->tf->nf; i++) {
+		printf("%zu\t%u\t\n", i, (unsigned int)ctx->tf->f[i]);
+	}
+
+	free_corpus(ctx->c);
+	free(ctx->tf);
 out:
 	glod_parser_free(argi);
 	return res;
