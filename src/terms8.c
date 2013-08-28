9df26ddd4a1842dc7838bc774b8eb5b7b67a0fdf
Sebastian Freundt <freundt@ga-group.nl>
2013-08-28 15:55:18 +0000

    minor, try Bruno Haible's libutf8 and provide a terms8 that is fixed to alnum chars
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ src/terms8.c
@@ -0,0 +1,174 @@
+/*** terms8.c -- extract terms from utf8 sources
+ *
+ * Copyright (C) 2013 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of glod.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <utf8/stdlib.h>
+#include <utf8/wchar.h>
+#include <utf8/wctype.h>
+#include <utf8/locale.h>
+#include "nifty.h"
+#include "fops.h"
+
+typedef uint_fast8_t clsf_streak_t;
+
+
+static void
+__attribute__((format(printf, 1, 2)))
+error(const char *fmt, ...)
+{
+	va_list vap;
+	va_start(vap, fmt);
+	vfprintf(stderr, fmt, vap);
+	va_end(vap);
+	if (errno) {
+		fputc(':', stderr);
+		fputc(' ', stderr);
+		fputs(strerror(errno), stderr);
+	}
+	fputc('\n', stderr);
+	return;
+}
+
+
+static void
+pr_strk(const char *tp, size_t strkz)
+{
+/* print the streak of size STRKZ that ends on tail-pointer TP. */
+	fwrite(tp - strkz, sizeof(*tp), strkz, stdout);
+	fputc('\n', stdout);
+	return;
+}
+
+static int
+classify_buf(const char *buf, size_t z)
+{
+	clsf_streak_t strk = 0U;
+	wchar_t c[1];
+	int n;
+
+	for (const char *bp = buf, *const ep = bp + z;
+	     bp < ep && (n = mbtowc(c, bp, ep - bp)) > 0; bp += n) {
+		if (LIKELY(iswalnum(*c))) {
+			strk += (uint8_t)n;
+		} else if (LIKELY(strk)) {
+			pr_strk(bp, strk);
+			strk = 0U;
+		}
+	}
+	return 0;
+}
+
+static int
+classify1(const char *fn)
+{
+	glodfn_t f;
+	int res = -1;
+
+	/* map the file FN and snarf the alerts */
+	if (UNLIKELY((f = mmap_fn(fn, O_RDONLY)).fd < 0)) {
+		goto out;
+	}
+
+	/* peruse */
+	if (classify_buf(f.fb.d, f.fb.z) < 0) {
+		goto out;
+	}
+
+	/* we printed our findings by side-effect already,
+	 * finalise the output here */
+	puts("\f");
+
+	/* total success innit? */
+	res = 0;
+
+out:
+	(void)munmap_fn(f);
+	return res;
+}
+
+
+#if defined __INTEL_COMPILER
+# pragma warning (disable:593)
+# pragma warning (disable:181)
+#endif	/* __INTEL_COMPILER */
+#include "terms.xh"
+#include "terms.x"
+#if defined __INTEL_COMPILER
+# pragma warning (default:593)
+# pragma warning (default:181)
+#endif	/* __INTEL_COMPILER */
+
+int
+main(int argc, char *argv[])
+{
+	struct glod_args_info argi[1];
+	int res;
+
+	if (glod_parser(argc, argv, argi)) {
+		res = 1;
+		goto out;
+	} else if (argi->inputs_num < 1) {
+		fputs("Error: no FILE given\n\n", stderr);
+		glod_parser_print_help();
+		res = 1;
+		goto out;
+	}
+
+	/* pretend! */
+	locale_is_utf8 = 1;
+	/* run stats on that one file */
+	for (unsigned int i = 0; i < argi->inputs_num; i++) {
+		const char *file = argi->inputs[i];
+
+		if ((res = classify1(file)) < 0) {
+			error("Error: processing `%s' failed", file);
+		}
+	}
+
+out:
+	glod_parser_free(argi);
+	return res;
+}
+
+/* terms8.c ends here */
