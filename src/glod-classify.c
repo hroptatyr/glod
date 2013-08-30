a9990416cfde66f4926e241b8ebe8b6db2d99549
Sebastian Freundt <freundt@ga-group.nl>
2013-08-30 15:25:48 +0000

    hygiene, define REFCLASSIFIER in a way so that even gcc can cope with it
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/glod-classify.c
+++ src/glod-classify.c
@@ -68,7 +68,7 @@ struct classifier_s {
 static unsigned int				\
 cls_##name(const char *buf, size_t len)
 
-#define REFCLASSIFIER(name)	(struct classifier_s){#name, cls_##name}
+#define REFCLASSIFIER(name)	{#name, cls_##name, 0}
 
 #define CLASSIFIER(name, b, z)	cls_##name(b, z)
 
