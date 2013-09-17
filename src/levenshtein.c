4a8edc50e08a5599106ed63ffbbb5022b7fcf77c
Sebastian Freundt <freundt@ga-group.nl>
2013-09-17 07:20:03 +0000

    Revert "try with bose-nelson"
    This reverts commit f7132815f1c5eb6f6e4c2e7c6d3da179c9eb72ef.
    
    Somewhat slower.

--- src/levenshtein.c
+++ src/levenshtein.c
@@ -93,57 +93,35 @@ ldcalc(const char *s1, size_t z1, const char *s2, size_t z2, ld_opt_t o)
 	for (size_t j = 0U; j <= z2; j++) {
 		row1[j] = j * PNLTY(insdel);
 	}
-	for (size_t i = 0U; i < z1;
-	     ({
-		     with (dist_t *dummy = row0) {
-			     row0 = row1;
-			     row1 = row2;
-			     row2 = dummy;
-		     };
-		     i++;})) {
-		/* insertion afront */
+	for (size_t i = 0U; i < z1; i++) {
 		row2[0] = (i + 1U) * PNLTY(insdel);
-
-		for (size_t j = 1U; j <= z2; j++) {
-			dist_t ds;
-			dist_t dt;
-			dist_t di;
-			dist_t dd;
-
+		for (size_t j = 0U; j < z2; j++) {
 			/* substitution */
-			ds = row1[j - 1] + PNLTY(subst) * (s1[i] != s2[j - 1U]);
+			row2[j + 1U] = row1[j] + PNLTY(subst) * (s1[i] != s2[j]);
 
-			/* transpose, needs i > 0 */
-			if (UNLIKELY(i == 0U || j == 1U)) {
-				dt = ds;
-			} else if (s1[i - 1U] != s2[j - 1U] ||
-				   s1[i - 0U] != s2[j - 2U]) {
-				dt = ds;
-			} else {
-				dt = row0[j - 2U] + PNLTY(trnsp);
+			/* swap */
+			if (i > 0 && j > 0 &&
+			    s1[i - 1U] == s2[j - 0U] &&
+			    s1[i - 0U] == s2[j - 1U] &&
+			    row2[j + 1U] > row0[j - 1U] + PNLTY(trnsp)) {
+				row2[j + 1U] = row0[j - 1U] + PNLTY(trnsp);
 			}
 
 			/* deletion */
-			dd = row1[j] + PNLTY(insdel);
+			if (row2[j + 1U] > row1[j + 1U] + PNLTY(insdel)) {
+				row2[j + 1U] = row1[j + 1U] + PNLTY(insdel);
+			}
 
-			/* insertion, reuses row2 values */
-			di = row2[j - 1U] + PNLTY(insdel);
+			/* insertion */
+			if (row2[j + 1U] > row2[j + 0U] + PNLTY(insdel)) {
+				row2[j + 1U] = row2[j + 0U] + PNLTY(insdel);
+			}
+		}
 
-			/* get the minimum and ass, using a bose-nelson
-			 * sorting network here:
-			 * [[0,1],[2,3]]
-			 * [[0,2],[1,3]]
-			 * [[1,2]] */
-			if (dt < ds) {
-				ds = dt;
-			}
-			if (dd < di) {
-				di = dd;
-			}
-			if (di < ds) {
-				ds = di;
-			}
-			row2[j] = ds;
+		with (dist_t *dummy = row0) {
+			row0 = row1;
+			row1 = row2;
+			row2 = dummy;
 		}
 	}
 
