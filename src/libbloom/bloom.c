968793c8acfe80761e855d07df071a382d365783
Sebastian Freundt <freundt@ga-group.nl>
2014-06-02 11:40:21 +0000

    hygiene, clean up libbloom's bloom.[ch]
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/libbloom/bloom.c
+++ src/libbloom/bloom.c
@@ -8,64 +8,70 @@
 #include "murmur.h"
 #include "spooky.h"
 
+/**
+ * We use a magic header to identify the bloom filters.
+ */
+struct bloom_filter_header {
+	uint32_t magic;     // Magic 4 bytes
+	uint32_t k_num;     // K_num value
+	uint64_t count;     // Count of items
+	char __buf[496];    // Pad out to 512 bytes
+} __attribute__((packed));
+
+
 /*
  * Static definitions
  */
 static const uint32_t MAGIC_HEADER = 0xCB1005DD;  // Vaguely like CBLOOMDD
 
-/**
- * Creates a new bloom filter using a given bitmap and k-value.
- * @arg map A bloom_bitmap pointer.
- * @arg k_num The number of hash functions to use. Ignored if the header value is different.
- * @arg new_filter 1 if new, sets the magic byte and does not check it.
- * @arg filter The filter to setup
- * @return 0 for success. Negative for error.
- */
-int bf_from_bitmap(bloom_bitmap *map, uint32_t k_num, int new_filter, bloom_bloomfilter *filter) {
-    // Check our args
-    if (map == NULL or k_num < 1) {
-        return -EINVAL;
-    }
+static const double log2sq = 0.6931471805599453 * 0.6931471805599453;
 
-    // Check the size of the map
-    if (map->size < sizeof(bloom_filter_header)) {
-        return -ENOMEM;
-    }
+// Computes our hashes
+static void
+bf_compute_hashes(uint64_t *restrict hashes, unsigned int k_num, char *key)
+{
+	/**
+	 * We use the results of
+	 * 'Less Hashing, Same Performance: Building a Better Bloom Filter'
+	 * http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf, to use
+	 * g_i(x) = h1(u) + i * h2(u) mod m'
+	 *
+	 * This allows us to only use 2 hash functions h1, and h2 but generate
+	 * k unique hashes using linear combinations. This is a vast speedup
+	 * over our previous technique of 4 hashes, that used double hashing.
+	 *
+	 */
 
-    // Setup the pointers
-    filter->map = map;
-    filter->header = (bloom_filter_header*)map->mmap;
+	// Get the length of the key
+	size_t len = strlen(key);
 
-    // Get the bitmap size
-    filter->bitmap_size = (map->size - sizeof(bloom_filter_header)) * 8;
+	// Compute the first hash
+	uint64_t out[2];
+	MurmurHash3_x64_128(key, len, 0, out);
 
-    // Setup the header if it is new
-    if (new_filter) {
-        filter->header->magic = MAGIC_HEADER;
-        filter->header->k_num = k_num;
-        filter->header->count = 0;
+	// Copy these out
+	hashes[0] = out[0];  // Upper 64bits of murmur
+	hashes[1] = out[1];  // Lower 64bits of murmur
 
-        // Since this is a new filter, force a flush of
-        // the headers. This mainly affects bitmaps that
-        // are in the PERSIST mode. Since no flush happens
-        // until the first key is set, it can cause filters
-        // to be created that have no headers, and thus cannot
-        // be loaded.
-        bf_flush(filter);
+	// Compute the second hash
+	uint64_t *hash1 = out + 0;
+	uint64_t *hash2 = out + 1;
+	SpookyHash128(key, len, hash1, hash2);
 
-    // Check for the header if not new
-    } else if (filter->header->magic != MAGIC_HEADER) {
-        syslog(LOG_ERR, "Magic byte for bloom filter is wrong! Aborting load.");
-        return -1;
-    }
+	// Copy these out
+	hashes[2] = out[0];   // Use the upper 64bits of Spooky
+	hashes[3] = out[1];   // Use the lower 64bits of Spooky
 
-    // Setup the offset
-    filter->offset = filter->bitmap_size / filter->header->k_num;
-
-    // Done, return
-    return 0;
+	// Compute an arbitrary k_num using a linear combination
+	// Add a mod by the largest 64bit prime. This only reduces the
+	// number of addressable bits by 54 but should make the hashes
+	// a bit better.
+	for (size_t i = 4U; i < k_num; i++) {
+		hashes[i] = hashes[1] +
+			((i * hashes[3]) % 18446744073709551557ULL);
+	}
+	return;
 }
-
 
 /**
  * Internal bf_contains method.
@@ -74,26 +80,101 @@ int bf_from_bitmap(bloom_bitmap *map, uint32_t k_num, int new_filter, bloom_bloo
  * @arg hashes Contains at least K num hashes
  * @return 0 if not contained, 1 if contained.
  */
-static int bf_internal_contains(bloom_bloomfilter *filter, uint64_t *hashes) {
-    uint64_t m = filter->offset;
-    uint64_t offset;
-    uint64_t h;
-    uint32_t i;
-    uint64_t bit;
-    int res;
+static int
+bf_internal_contains(bloom_filter *filter, const uint64_t *hashes)
+{
+	const uint64_t m = filter->offset;
 
-    for (i=0; i< filter->header->k_num; i++) {
-        h = hashes[i];                                  // Get the hash value
-        offset = 8*sizeof(bloom_filter_header) + i * m; // Get the partition offset
-        bit = offset + (h % m);                         // Compute the bit offset
-        res = bitmap_getbit(filter->map, bit);
-        if (res == 0) {
-            return 0;
-        }
-    }
-    return 1;
+	for (size_t i = 0; i < filter->header->k_num; i++) {
+		/* get hash value */
+		const uint64_t h = hashes[i];
+		/* and partition offset */
+		const uint64_t offset =
+			BITS_PER_UF8 * sizeof(filter->header) + i * m;
+		/* Compute the bit offset */
+		const idx_t bit = offset + (h % m);
+
+		if (!bitmap_getbit(filter->map, bit)) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
+
+/* public API */
+/**
+ * Initialises filter internals using a given bitmap and k-value.
+ * @arg map A bloom_bitmap pointer.
+ * @arg k_num If non-zero set the number of hash functions to use.
+ * @return 0 for success. Negative for error.
+ */
+int
+bf_init(bloom_bitmap *map, unsigned int k_num)
+{
+	struct bloom_filter_header h = {
+		MAGIC_HEADER,
+		k_num,
+		0U,
+	};
+
+	/* check values for k_num and the size of the map */
+	if (map == NULL || k_num < 1) {
+		errno = EINVAL;
+		return -1;
+	} else if (map->size < sizeof(h)) {
+		errno = ENOMEM;
+		return -1;
+	}
+
+	/* set header in map */
+	memcpy(map, &h, sizeof(h));
+
+	/* Since this is a new filter, force a flush of
+	 * the headers. This mainly affects bitmaps that
+	 * are in the PERSIST mode. Since no flush happens
+	 * until the first key is set, it can cause filters
+	 * to be created that have no headers, and thus cannot
+	 * be loaded. */
+	bitmap_flush(map);
+	return 0;
+}
+
+/**
+ * Creates a new bloom filter using a given bitmap and k-value.
+ * @arg map A bloom_bitmap pointer.
+ * @arg filter The filter to setup
+ * @return 0 for success. Negative for error.
+ */
+int
+bf_from_bitmap(bloom_bitmap *map, bloom_filter *filter)
+{
+	// Check our args
+	if (map == NULL) {
+		return -EINVAL;
+	} else if (map->size < sizeof(*filter->header)) {
+		return -ENOMEM;
+	}
+
+	// Setup the pointers
+	filter->map = map;
+	filter->header = (void*)map->mmap;
+
+	// Get the bitmap size
+	filter->bitmap_size = (map->size - sizeof(*filter->header)) * 8U;
+
+	/* check header */
+	if (filter->header->magic != MAGIC_HEADER) {
+		errno = EBADMSG;
+		return -1;
+	}
+
+	// Setup the offset
+	filter->offset = filter->bitmap_size / filter->header->k_num;
+
+	// Done, return
+	return 0;
+}
 
 /**
  * Adds a new key to the bloom filter.
@@ -101,34 +182,36 @@ static int bf_internal_contains(bloom_bloomfilter *filter, uint64_t *hashes) {
  * @arg key The key to add
  * @returns 1 if the key was added, 0 if present. Negative on failure.
  */
-int bf_add(bloom_bloomfilter *filter, char* key) {
-    // Allocate the hash space
-    uint64_t *hashes = alloca(filter->header->k_num * sizeof(uint64_t));
+int bf_add(bloom_filter *filter, char* key)
+{
+	// Allocate the hash space
+	uint64_t hashes[filter->header->k_num * sizeof(uint64_t)];
+	int rc;
 
-    // Compute the hashes
-    bf_compute_hashes(filter->header->k_num, key, hashes);
+	// Compute the hashes
+	bf_compute_hashes(hashes, filter->header->k_num, key);
 
-    // Check if the item exists
-    int res = bf_internal_contains(filter, hashes);
-    if (res == 1) {
-        return 0;  // Key already present, do not add.
-    }
+	// Check if the item exists
+	if ((rc = bf_internal_contains(filter, hashes)) == 1) {
+		/* Key already present, do not add. */
+		return 0;
+	}
 
-    uint64_t m = filter->offset;
-    uint64_t offset;
-    uint64_t h;
-    uint32_t i;
-    uint64_t bit;
+	const uint64_t m = filter->offset;
+	for (size_t i = 0U; i < filter->header->k_num; i++) {
+		const uint64_t h = hashes[i];
+		/* get the partition offset */
+		const uint64_t offset =
+			BITS_PER_UF8 * sizeof(*filter->header) + i * m;
+		/* compute actual bit */
+		const idx_t bit = offset + (h % m);
 
-    for (i=0; i< filter->header->k_num; i++) {
-        h = hashes[i];                                  // Get the hash value
-        offset = 8*sizeof(bloom_filter_header) + i * m; // Get the partition offset
-        bit = offset + (h % m);                         // Compute the bit offset
-        bitmap_setbit(filter->map, bit);
-    }
+		/* set the bit */
+		bitmap_setbit(filter->map, bit);
+	}
 
-    filter->header->count += 1;
-    return 1;
+	filter->header->count++;
+	return 1;
 }
 
 /**
@@ -137,35 +220,38 @@ int bf_add(bloom_bloomfilter *filter, char* key) {
  * @arg key The key to check
  * @returns 1 if present, 0 if not present, negative on error.
  */
-int bf_contains(bloom_bloomfilter *filter, char* key) {
-    // Allocate the hash space
-    uint64_t *hashes = alloca(filter->header->k_num * sizeof(uint64_t));
+int bf_contains(bloom_filter *filter, char* key)
+{
+	// Allocate the hash space
+	uint64_t hashes[filter->header->k_num * sizeof(uint64_t)];
 
-    // Compute the hashes
-    bf_compute_hashes(filter->header->k_num, key, hashes);
+	// Compute the hashes
+	bf_compute_hashes(hashes, filter->header->k_num, key);
 
-    // Use the internal contains method
-    return bf_internal_contains(filter, hashes);
+	// Use the internal contains method
+	return bf_internal_contains(filter, hashes);
 }
 
 /**
  * Returns the size of the bloom filter in item count
  */
-uint64_t bf_size(bloom_bloomfilter *filter) {
-    // Read it from the file header directly
-    return filter->header->count;
+uint64_t bf_size(bloom_filter *filter)
+{
+	// Read it from the file header directly
+	return filter->header->count;
 }
 
 /**
  * Flushes the filter, and updates the metadata.
  * @return 0 on success, negative on failure.
  */
-int bf_flush(bloom_bloomfilter *filter) {
-    // Flush the bitmap if we have one
-    if (filter == NULL or filter->map == NULL) {
-        return -1;
-    }
-    return bitmap_flush(filter->map);
+int bf_flush(bloom_filter *filter)
+{
+	// Flush the bitmap if we have one
+	if (filter == NULL || filter->map == NULL) {
+		return -1;
+	}
+	return bitmap_flush(filter->map);
 }
 
 /**
@@ -173,25 +259,25 @@ int bf_flush(bloom_bloomfilter *filter) {
  * but does not free it.
  * @return 0 on success, negative on failure.
  */
-int bf_close(bloom_bloomfilter *filter) {
-    // Make sure we have a filter
-    if (filter == NULL or filter->map == NULL) {
-        return -1;
-    }
+int bf_close(bloom_filter *filter)
+{
+	// Make sure we have a filter
+	if (filter == NULL || filter->map == NULL) {
+		return -1;
+	}
 
-    // Flush first
-    bf_flush(filter);
+	// Flush first
+	bf_flush(filter);
 
-    // Clean up the map
-    bitmap_close(filter->map);
-    filter->map = NULL;
+	// Clean up the map
+	bitmap_close(filter->map);
+	filter->map = NULL;
 
-    // Clear all the fields
-    filter->header = NULL;
-    filter->offset = 0;
-    filter->bitmap_size = 0;
-
-    return 0;
+	// Clear all the fields
+	filter->header = NULL;
+	filter->offset = 0;
+	filter->bitmap_size = 0;
+	return 0;
 }
 
 /*
@@ -203,18 +289,23 @@ int bf_close(bloom_bloomfilter *filter) {
  * and sets the bytes and k_num that should be used.
  * @return 0 on success, negative on error.
  */
-int bf_params_for_capacity(bloom_filter_params *params) {
-    // Sets the required size
-    int res = bf_size_for_capacity_prob(params);
-    if (res != 0) return res;
+int bf_params_for_capacity(bloom_filter_params *params)
+{
+	// Sets the required size
+	int res = bf_size_for_capacity_prob(params);
 
-    // Sets the ideal k
-    res = bf_ideal_k_num(params);
-    if (res != 0) return res;
+	if (res != 0) {
+		return res;
+	}
 
-    // Adjust for the header size
-    params->bytes += sizeof(bloom_filter_header);
-    return 0;
+	// Sets the ideal k
+	res = bf_ideal_k_num(params);
+	if (res != 0) {
+		return res;
+	}
+	// Adjust for the header size
+	params->bytes += sizeof(struct bloom_filter_header);
+	return 0;
 }
 
 /*
@@ -222,33 +313,37 @@ int bf_params_for_capacity(bloom_filter_params *params) {
  * minimum byte size required.
  * @return 0 on success, negative on error.
  */
-int bf_size_for_capacity_prob(bloom_filter_params *params) {
-    uint64_t capacity = params->capacity;
-    double fp_prob = params->fp_probability;
-    if (capacity == 0 or fp_prob == 0) {
-        return -1;
-    }
-    double bits = -(capacity*log(fp_prob)/(log(2)*log(2)));
-    uint64_t whole_bits = ceil(bits);
-    params->bytes = ceil(whole_bits / 8.0);
-    return 0;
-}
+int bf_size_for_capacity_prob(bloom_filter_params *params)
+{
+	uint64_t capacity = params->capacity;
+	double fp_prob = params->fp_probability;
 
+	if (capacity == 0 || fp_prob == 0.0) {
+		return -1;
+	}
+	double bits = -(capacity * log(fp_prob) / log2sq);
+	uint64_t whole_bits = ceil(bits);
+	params->bytes = ceil(whole_bits / 8.0);
+	return 0;
+}
 
 /*
  * Expects capacity and size to be set, computes the best
  * false positive probability given an ideal k.
  * @return 0 on success, negative on error.
  */
-int bf_fp_probability_for_capacity_size(bloom_filter_params *params) {
-    uint64_t bits = params->bytes * 8;
-    uint64_t capacity = params->capacity;
-    if (bits == 0 or capacity == 0) {
-        return -1;
-    }
-    double fp_prob = pow(M_E, -( (double)bits / (double)capacity)*(pow(log(2),2)));
-    params->fp_probability = fp_prob;
-    return 0;
+int bf_fp_probability_for_capacity_size(bloom_filter_params *params)
+{
+	uint64_t bits = params->bytes * 8;
+	uint64_t capacity = params->capacity;
+	double fp_prob;
+
+	if (bits == 0 || capacity == 0) {
+		return -1;
+	}
+	fp_prob = pow(M_E, -( (double)bits / (double)capacity) * log2sq);
+	params->fp_probability = fp_prob;
+	return 0;
 }
 
 /*
@@ -256,15 +351,18 @@ int bf_fp_probability_for_capacity_size(bloom_filter_params *params) {
  * computes the expected capacity.
  * @return 0 on success, negative on error.
  */
-int bf_capacity_for_size_prob(bloom_filter_params *params) {
-    uint64_t bits = params->bytes * 8;
-    double prob = params->fp_probability;
-    if (bits == 0 or prob == 0) {
-        return -1;
-    }
-    uint64_t capacity = -(bits / log(prob) * (log(2) * log(2)));
-    params->capacity = capacity;
-    return 0;
+int bf_capacity_for_size_prob(bloom_filter_params *params)
+{
+	uint64_t bits = params->bytes * 8;
+	double prob = params->fp_probability;
+	uint64_t capacity;
+
+	if (bits == 0 || prob == 0.0) {
+		return -1;
+	}
+	capacity = (uint64_t)-(bits / log(prob) * log2sq);
+	params->capacity = capacity;
+	return 0;
 }
 
 /*
@@ -272,57 +370,18 @@ int bf_capacity_for_size_prob(bloom_filter_params *params) {
  * computes the ideal k num.
  * @return 0 on success, negative on error.
  */
-int bf_ideal_k_num(bloom_filter_params *params) {
-    uint64_t bits = params->bytes * 8;
-    uint64_t capacity = params->capacity;
-    if (bits == 0 or capacity == 0) {
-        return -1;
-    }
-    uint32_t ideal_k = round(log(2) * bits / capacity);
-    params->k_num = ideal_k;
-    return 0;
+int bf_ideal_k_num(bloom_filter_params *params)
+{
+	uint64_t bits = params->bytes * 8;
+	uint64_t capacity = params->capacity;
+	uint32_t ideal_k;
+
+	if (bits == 0 || capacity == 0) {
+		return -1;
+	}
+	ideal_k = (uint32_t)round(log(2) * bits / capacity);
+	params->k_num = ideal_k;
+	return 0;
 }
 
-// Computes our hashes
-void bf_compute_hashes(uint32_t k_num, char *key, uint64_t *hashes) {
-    /**
-     * We use the results of
-     * 'Less Hashing, Same Performance: Building a Better Bloom Filter'
-     * http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf, to use
-     * g_i(x) = h1(u) + i * h2(u) mod m'
-     *
-     * This allows us to only use 2 hash functions h1, and h2 but generate
-     * k unique hashes using linear combinations. This is a vast speedup
-     * over our previous technique of 4 hashes, that used double hashing.
-     *
-     */
-
-    // Get the length of the key
-    uint64_t len = strlen(key);
-
-    // Compute the first hash
-    uint64_t out[2];
-    MurmurHash3_x64_128(key, len, 0, out);
-
-    // Copy these out
-    hashes[0] = out[0];  // Upper 64bits of murmur
-    hashes[1] = out[1];  // Lower 64bits of murmur
-
-    // Compute the second hash
-    uint64_t *hash1 = out + 0;
-    uint64_t *hash2 = out + 1;
-    SpookyHash128(key, len, hash1, hash2);
-
-    // Copy these out
-    hashes[2] = out[0];   // Use the upper 64bits of Spooky
-    hashes[3] = out[1];   // Use the lower 64bits of Spooky
-
-    // Compute an arbitrary k_num using a linear combination
-    // Add a mod by the largest 64bit prime. This only reduces the
-    // number of addressable bits by 54 but should make the hashes
-    // a bit better.
-    for (uint32_t i=4; i < k_num; i++) {
-        hashes[i] = hashes[1] + ((i * hashes[3]) % 18446744073709551557ULL);
-    }
-}
-
+/* bloom.c ends here */
--- src/libbloom/bloom.h
+++ src/libbloom/bloom.h
@@ -6,48 +6,48 @@
 #include <errno.h>
 #include "bitmap.h"
 
-/**
- * We use a magic header to identify the bloom filters.
- */
-struct bloom_filter_header {
-    uint32_t magic;     // Magic 4 bytes
-    uint32_t k_num;     // K_num value
-    uint64_t count;     // Count of items
-    char __buf[496];     // Pad out to 512 bytes
-} __attribute__ ((packed));
-typedef struct bloom_filter_header bloom_filter_header;
-
 /*
  * This is the struct we use to represent a bloom filter.
  */
 typedef struct {
-    bloom_filter_header *header;   // Pointer to the header in the bitmap region
-    bloom_bitmap *map;             // Underlying bitmap
-    uint64_t offset;                // The offset size between hash regions
-    uint64_t bitmap_size;           // The size of the bitmap to use, minus buffers
-} bloom_bloomfilter;
+	// Pointer to the header in the bitmap region
+	struct bloom_filter_header *header;
+	// Underlying bitmap
+	bloom_bitmap *map;
+	// The offset size between hash regions
+	uint64_t offset;
+	// The size of the bitmap to use, minus buffers
+	uint64_t bitmap_size;
+} bloom_filter;
 
 /*
  * Structure used to store the parameter information
  * for configuring bloom filters.
  */
 typedef struct {
-    uint64_t bytes;
-    uint32_t k_num;
-    uint64_t capacity;
-    double   fp_probability;
+	uint64_t bytes;
+	uint32_t k_num;
+	uint64_t capacity;
+	double   fp_probability;
 } bloom_filter_params;
 
 
 /**
- * Creates a new bloom filter using a given bitmap and k-value.
+ * Initialises filter internals using a given bitmap and k-value.
  * @arg map A bloom_bitmap pointer.
- * @arg k_num The number of hash functions to use. Ignored if the header value is different.
- * @arg new_filter 1 if new, sets the magic byte and does not check it.
+ * @arg k_num If non-zero set the number of hash functions to use.
+ * @return 0 for success. Negative for error.
+ */
+extern int bf_init(bloom_bitmap *map, unsigned int k_num);
+
+/**
+ * Creates a new bloom filter using a given (initialised) bitmap.
+ * @arg map A bloom_bitmap pointer.
  * @arg filter The filter to setup
  * @return 0 for success. Negative for error.
  */
-int bf_from_bitmap(bloom_bitmap *map, uint32_t k_num, int new_filter, bloom_bloomfilter *filter);
+extern int
+bf_from_bitmap(bloom_bitmap *map, bloom_filter *filt);
 
 /**
  * Adds a new key to the bloom filter.
@@ -55,7 +55,7 @@ int bf_from_bitmap(bloom_bitmap *map, uint32_t k_num, int new_filter, bloom_bloo
  * @arg key The key to add
  * @returns 1 if the key was added, 0 if present. Negative on failure.
  */
-int bf_add(bloom_bloomfilter *filter, char* key);
+extern int bf_add(bloom_filter *filter, char* key);
 
 /**
  * Checks the filter for a key
@@ -63,33 +63,25 @@ int bf_add(bloom_bloomfilter *filter, char* key);
  * @arg key The key to check
  * @returns 1 if present, 0 if not present, negative on error.
  */
-int bf_contains(bloom_bloomfilter *filter, char* key);
+extern int bf_contains(bloom_filter *filter, char* key);
 
 /**
  * Returns the size of the bloom filter in item count
  */
-uint64_t bf_size(bloom_bloomfilter *filter);
+extern size_t bf_size(bloom_filter *filter);
 
 /**
  * Flushes the filter, and updates the metadata.
  * @return 0 on success, negative on failure.
  */
-int bf_flush(bloom_bloomfilter *filter);
+extern int bf_flush(bloom_filter *filter);
 
 /**
  * Flushes and closes the filter. Closes the underlying bitmap,
  * but does not free it.
  * @return 0 on success, negative on failure.
  */
-int bf_close(bloom_bloomfilter *filter);
-
-/*
- * Computes the hashes for a bloom filter
- * @arg k_num the number of hashes to compute
- * @arg key The key to hash
- * @arg hashes Array to write to
- */
-void bf_compute_hashes(uint32_t k_num, char *key, uint64_t *hashes);
+extern int bf_close(bloom_filter *filter);
 
 /*
  * Utility methods for computing parameters
@@ -101,35 +93,35 @@ void bf_compute_hashes(uint32_t k_num, char *key, uint64_t *hashes);
  * This byte size accounts for the headers we need.
  * @return 0 on success, negative on error.
  */
-int bf_params_for_capacity(bloom_filter_params *params);
+extern int bf_params_for_capacity(bloom_filter_params *params);
 
 /*
  * Expects capacity and probability to be set, computes the
  * minimum byte size required. Does not include header size.
  * @return 0 on success, negative on error.
  */
-int bf_size_for_capacity_prob(bloom_filter_params *params);
+extern int bf_size_for_capacity_prob(bloom_filter_params *params);
 
 /*
  * Expects capacity and size to be set, computes the best
  * false positive probability given an ideal k.
  * @return 0 on success, negative on error.
  */
-int bf_fp_probability_for_capacity_size(bloom_filter_params *params);
+extern int bf_fp_probability_for_capacity_size(bloom_filter_params *params);
 
 /*
  * Expects bytes and probability to be set,
  * computes the expected capacity.
  * @return 0 on success, negative on error.
  */
-int bf_capacity_for_size_prob(bloom_filter_params *params);
+extern int bf_capacity_for_size_prob(bloom_filter_params *params);
 
 /*
  * Expects bytes and capacity to be set,
  * computes the ideal k num.
  * @return 0 on success, negative on error.
  */
-int bf_ideal_k_num(bloom_filter_params *params);
+extern int bf_ideal_k_num(bloom_filter_params *params);
 
 #endif
 
