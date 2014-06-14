3613882d2adbc9deb568824659eb4261840173d8
Sebastian Freundt <freundt@ga-group.nl>
2014-06-14 06:57:54 +0000

    minor, allow enum(1) to load and save its state
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/enum.c
+++ src/enum.c
@@ -37,14 +37,18 @@
 #if defined HAVE_CONFIG_H
 # include "config.h"
 #endif	/* HAVE_CONFIG_H */
+#include <unistd.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdio.h>
 #include "nifty.h"
 
-typedef uint_fast32_t obint_t;
+typedef uint32_t obint_t;
 
 /* a hash is the bucket locator and a chksum for collision detection */
 typedef struct {
@@ -52,10 +56,14 @@ typedef struct {
 	uint_fast32_t chk;
 } hash_t;
 
+/* for materialisation to file */
+struct hdr_s {
+};
+
 /* the beef table */
 static struct {
 	obint_t ob;
-	uint_fast32_t ck;
+	uint32_t ck;
 } *sstk;
 /* alloc size, 2-power */
 static size_t zstk;
@@ -65,6 +73,25 @@ static size_t nstk;
 /* next ob number */
 static size_t obn;
 
+
+static void
+__attribute__((format(printf, 1, 2)))
+error(const char *fmt, ...)
+{
+	va_list vap;
+	va_start(vap, fmt);
+	vfprintf(stderr, fmt, vap);
+	va_end(vap);
+	if (errno) {
+		fputc(':', stderr);
+		fputc(' ', stderr);
+		fputs(strerror(errno), stderr);
+	}
+	fputc('\n', stderr);
+	return;
+}
+
+
 static hash_t
 murmur(const uint8_t *str, size_t len)
 {
@@ -161,6 +188,28 @@ enum0(void)
 	return 0;
 }
 
+static int
+load(const char *fn)
+{
+	return 0;
+}
+
+static int
+save(const char *fn)
+{
+	int fd;
+	int rc = 0;
+
+	if ((fd = open(fn, O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) {
+		return -1;
+	}
+	for (ssize_t nwr, tot = 0U;
+	     (nwr = write(fd, (char*)sstk + tot, zstk - tot)) > 0; tot += nwr);
+
+	close(fd);
+	return rc;
+}
+
 
 #if defined STANDALONE
 # include "enum.yucc"
@@ -169,6 +218,7 @@ int
 main(int argc, char *argv[])
 {
 	yuck_t argi[1U];
+	const char *fn = ".enum.st";
 	int rc = 0;
 
 	if (yuck_parse(argi, argc, argv)) {
@@ -176,10 +226,31 @@ main(int argc, char *argv[])
 		goto out;
 	}
 
-	rc = enum0();
-	clear_enums();
+	if (argi->file_arg) {
+		fn = argi->file_arg;
+	}
+
+	if (0);
+
+	/* load the state */
+	else if (argi->stateful_flag && load(fn) < 0) {
+		error("Error: cannot load state from `%s'", fn);
+		rc = 1;
+	}
+
+	/* do the enumeration */
+	else if (enum0() < 0) {
+		rc = 1;
+	}
+
+	/* save the state */
+	else if (argi->stateful_flag && save(fn) < 0) {
+		rc = 1;
+	}
 
 out:
+	/* cleanup */
+	clear_enums();
 	yuck_free(argi);
 	return rc;
 }
