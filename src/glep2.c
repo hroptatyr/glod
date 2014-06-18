6448519a82fba286d98fe3fcc3016485daac8648
Sebastian Freundt <freundt@ga-group.nl>
2014-06-18 15:03:32 +0000

    minor, actually count the occurrences of the patterns
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/glep2.c
+++ src/glep2.c
@@ -40,6 +40,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <immintrin.h>
 #include "glep.h"
 #include "boobs.h"
 #include "intern.h"
@@ -79,6 +80,36 @@ static const char stdin_fn[] = "<stdin>";
 
 #define warn(x...)
 
+#if defined __AVX2__
+# define __mXi			__m256i
+# define _mmX_load_si(x)	_mm256_load_si256(x)
+# define _mmX_loadu_si(x)	_mm256_loadu_si256(x)
+# define _mmX_set1_epi8(x)	_mm256_set1_epi8(x)
+# define _mmX_setzero_si()	_mm256_setzero_si256()
+# define _mmX_cmpeq_epi8(x, y)	_mm256_cmpeq_epi8(x, y)
+# define _mmX_cmpgt_epi8(x, y)	_mm256_cmpgt_epi8(x, y)
+# define _mmX_cmplt_epi8(x, y)	_mm256_cmpgt_epi8(y, x)
+# define _mmX_add_si(x, y)	_mm256_add_epi8(x, y)
+# define _mmX_and_si(x, y)	_mm256_and_si256(x, y)
+# define _mmX_xor_si(x, y)	_mm256_xor_si256(x, y)
+# define _mmX_movemask_epi8(x)	_mm256_movemask_epi8(x)
+#elif defined __SSE2__
+# define __mXi			__m128i
+# define _mmX_load_si(x)	_mm_load_si128(x)
+# define _mmX_loadu_si(x)	_mm_loadu_si128(x)
+# define _mmX_set1_epi8(x)	_mm_set1_epi8(x)
+# define _mmX_setzero_si()	_mm_setzero_si128()
+# define _mmX_cmpeq_epi8(x, y)	_mm_cmpeq_epi8(x, y)
+# define _mmX_cmpgt_epi8(x, y)	_mm_cmpgt_epi8(x, y)
+# define _mmX_cmplt_epi8(x, y)	_mm_cmplt_epi8(x, y)
+# define _mmX_add_si(x, y)	_mm_add_epi8(x, y)
+# define _mmX_and_si(x, y)	_mm_and_si128(x, y)
+# define _mmX_xor_si(x, y)	_mm_xor_si128(x, y)
+# define _mmX_movemask_epi8(x)	_mm_movemask_epi8(x)
+#else
+# error need SIMD extensions of some sort
+#endif
+
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -103,6 +134,133 @@ error(const char *fmt, ...)
 	}
 	fputc('\n', stderr);
 	return;
+}
+
+static inline __attribute__((pure, const)) int
+pispuncs(register __mXi data)
+{
+/* looks for <=' ', '!', ',', '.', ':', ';', '?' '\'', '"', '`' */
+	register __mXi x0;
+	register __mXi x1;
+	register __mXi y0;
+	register __mXi y1;
+
+	/* check for <=SPC, !, " */
+	y0 = _mmX_cmplt_epi8(data, _mmX_set1_epi8('"' + 1));
+
+	/* check for '() */
+	x0 = _mmX_cmpgt_epi8(data, _mmX_set1_epi8('\'' - 1));
+	x1 = _mmX_cmplt_epi8(data, _mmX_set1_epi8(')' + 1));
+	y1 = _mmX_and_si(x0, x1);
+	y0 = _mmX_xor_si(y0, y1);
+
+	/* check for , and . */
+	x0 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8(','));
+	x1 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8('.'));
+	y1 = _mmX_xor_si(x0, x1);
+	y0 = _mmX_xor_si(y0, y1);
+
+	/* check for :; */
+	x0 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8(':'));
+	x1 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8(';'));
+	y1 = _mmX_xor_si(x0, x1);
+	y0 = _mmX_xor_si(y0, y1);
+
+	/* check for ?` */
+	x0 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8('?'));
+	x1 = _mmX_cmpeq_epi8(data, _mmX_set1_epi8('`'));
+	y1 = _mmX_xor_si(x0, x1);
+	y0 = _mmX_xor_si(y0, y1);
+
+	return _mmX_movemask_epi8(y0);
+}
+
+static inline __attribute__((pure, const)) __mXi
+ptolower(register __mXi data)
+{
+/* lower's standard ascii */
+	register __mXi x0;
+	register __mXi x1;
+	register __mXi y0;
+	register __mXi y1;
+
+	/* check for ALPHA */
+	x0 = _mmX_cmpgt_epi8(data, _mmX_set1_epi8('A' - 1));
+	x1 = _mmX_cmplt_epi8(data, _mmX_set1_epi8('Z' + 1));
+	y0 = _mmX_and_si(x0, x1);
+	y1 = _mmX_set1_epi8(32U);
+	y0 = _mmX_and_si(y0, y1);
+
+	return _mmX_add_si(data, y0);
+}
+
+
+static uint8_t *p1;
+static uint_fast32_t *c1;
+static size_t np1;
+
+static uint8_t *p2;
+static uint_fast32_t *c2;
+static size_t np2;
+
+static __attribute__((const, pure)) unsigned int
+isolw(const unsigned int sur, const unsigned int isol)
+{
+/* isolation weight, defined as
+ * a =  ...101
+ * b =  ...010
+ * c <- ...010
+ * meaning that if bits in b are "surrounded" by bits in a, then they're 1
+ *
+ * complete table would be:
+ * 0101U  1010U  1011U  10100U  10101U  10110U  10111U  10100U
+ * 0010U  0100U  0100U  01000U  01000U  01000U  01000U  01010U
+ * 0010U  0100U  0100U  01000U  01000U  01000U  01000U  01000U  etc.
+ *
+ * we build the complete table for 4 bits and shift a and b by 3. */
+	unsigned int isol_msk1 = 0b00010010010010010010010010010010U;
+	unsigned int isol_msk2 = 0b00100100100100100100100100100100U;
+	unsigned int isol_msk3 = 0b01001001001001001001001001001000U;
+
+	unsigned int sur_msk1 = 0b00101101101101101101101101101101U;
+	unsigned int sur_msk2 = 0b01011011011011011011011011011010U;
+	unsigned int sur_msk3 = 0b10110110110110110110110110110100U;
+
+	return ((sur & sur_msk1) >> 1U) & ((sur & sur_msk1) << 1U) &
+		(isol & isol_msk1) |
+		((sur & sur_msk2) >> 1U) & ((sur & sur_msk2) << 1U) &
+		(isol & isol_msk2) |
+		((sur & sur_msk3) >> 1U) & ((sur & sur_msk3) << 1U) &
+		(isol & isol_msk3);
+}
+
+static unsigned int
+p1cnt(register __mXi data, unsigned int punc)
+{
+	for (size_t i = 0U; i < np1; i++) {
+		register __mXi pat = _mmX_set1_epi8(p1[i]);
+		register __mXi x = _mmX_cmpeq_epi8(data, pat);
+		int r = _mmX_movemask_epi8(x);
+
+		c1[i] += _popcnt32(isolw(punc, r));
+	}
+	return 0U;
+}
+
+static unsigned int
+pcnt(const uint8_t *buf, size_t bsz)
+{
+	for (size_t i = 0U; i < bsz; i += sizeof(__mXi) - 1U) {
+		/* load */
+		register __mXi data = _mmX_loadu_si((const void*)(buf + i));
+		int p;
+
+		data = ptolower(data);
+		p = pispuncs(data);
+
+		p1cnt(data, p);
+	}
+	return 0U;
 }
 
 
@@ -160,6 +318,8 @@ DEFCORU(co_match, {
 
 	/* enter the main match loop */
 	do {
+		pcnt((const void*)buf, nrd);
+
 		/* now go through and scrape buffer portions off */
 		npr = (nrd / 16U) * 16U;
 	} while ((nrd = YIELD(npr)) > 0U);
@@ -206,6 +366,11 @@ match0(int fd)
 
 		assert(npr <= nrd);
 	} while (nrd > 0);
+
+	for (size_t i = 0U; i < np1; i++) {
+		printf("%zu\t%lu\n", i, c1[i]);
+	}
+	memset(c1, 0, np1 * sizeof(*c1));
 
 	UNPREP();
 	return res;
@@ -256,12 +421,45 @@ main(int argc, char *argv[])
 		goto out;
 	}
 
-	/* yay */
+	/* oki, rearrange patterns into 1grams, 2grams, 3,4grams, etc. */
 	for (size_t i = 0U; i < pf->npats; i++) {
 		const char *p = pf->pats[i].s;
+		const size_t z = strlen(p);
 
-		if (strlen(p) == 1U) {
-			puts(pf->pats[i].s);
+		switch (z) {
+		case 1U:
+			if (UNLIKELY(!(np1 % 64U))) {
+				/* resize */
+				const size_t nu = np1 + 64U;
+				p1 = realloc(p1, nu * sizeof(*p1));
+				c1 = realloc(c1, nu * sizeof(*c1));
+			}
+			if (UNLIKELY(*p >= 'A' && *p <= 'Z')) {
+				p1[np1++] = (uint8_t)(*p + 32U);
+			} else {
+				p1[np1++] = (uint8_t)*p;
+			}
+			break;
+		case 2U:
+			if (UNLIKELY(!(np2 % 128U))) {
+				/* resize */
+				const size_t nu = np2 + 128U;
+				p2 = realloc(p2, nu * sizeof(*p2));
+				c2 = realloc(c2, nu * sizeof(*c2));
+			}
+			if (UNLIKELY(p[0U] >= 'A' && p[0U] <= 'Z')) {
+				p2[np2++] = (uint8_t)(p[0U] + 32U);
+			} else {
+				p2[np2++] = (uint8_t)p[0U];
+			}
+			if (UNLIKELY(p[1U] >= 'A' && p[1U] <= 'Z')) {
+				p2[np2++] = (uint8_t)(p[1U] + 32U);
+			} else {
+				p2[np2++] = (uint8_t)p[1U];
+			}
+			break;
+		default:
+			break;
 		}
 	}
 
@@ -296,6 +494,14 @@ main(int argc, char *argv[])
 
 fr_gl:
 	/* resource hand over */
+	if (p1 != NULL) {
+		free(p1);
+		free(c1);
+	}
+	if (p2 != NULL) {
+		free(p2);
+		free(c2);
+	}
 	glep_fr(pf);
 	glod_fr_gleps(pf);
 out:
